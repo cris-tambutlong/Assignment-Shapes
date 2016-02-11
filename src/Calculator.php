077323ebbf2b1213fcd901b7554a762fe47e470f
cris-tambutlong <cris.tambutlong@gmail.com>
2016-02-11 18:46:20 +0800

    Update Calculator.php
    I've added a process to both surfaceArea and totalVolume methods to compute and display the total surface area and volume for shapes passed to these methods.

--- src/Calculator.php
+++ src/Calculator.php
@@ -7,12 +7,34 @@ class Calculator {
 	/**
 	 * Get the total surface area of all shapes
 	 *
-	 * @param array $shape
+	 * @param array $shapes
 	 * @return int
 	 */
 	public function surfaceArea(array $shapes)
 	{
-		return "undefined";
+		//Setting the initial total sum of area to zero (0).
+		$sum = 0;
+		
+		//Iterate through all the shapes and process
+		//them one by one.
+		foreach($shapes as $shape)
+		{
+			//We making sure we execute the correct area method 
+			//for each shape object. Normally, a 2D object computes
+			//for the area while a 3D object computes for surface area.
+			
+			if ($shape instanceof Shape3D)
+			{
+				$sum += $shape->surface_area();
+			}
+			else
+			{
+				$sum += $shape->area();
+			}
+		}	
+		
+		//Return the total sum of area.
+		return $sum;
 	}
 
 	/**
@@ -23,7 +45,23 @@ class Calculator {
 	 */
 	public function totalVolume(array $shapes)
 	{
-		return "undefined";
+		//Setting the initial total sum of volume to zero (0).
+		$sum = 0;
+		
+		//Iterate through all the shapes and process
+		//them one by one.
+		foreach($shapes as $shape)
+		{
+			//We making sure that we're filtering the shape to only 
+			//get the volume of 3D shape since 2D shape doesn't have volume. 
+			if ($shape instanceof Shape3D)
+			{
+				$sum += $shape->volume();
+			}
+		}
+		
+		//Return the total sum of volume.
+		return $sum;
 	}
 
 }
